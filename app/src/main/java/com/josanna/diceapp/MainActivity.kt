0239e67c99ec5ae409125f9fafb9a96bcb446b3c
Josanna <josanna@live.se>
2021-11-26 11:32:08 +0100

    Final commit

---
+++ app/src/main/java/com/josanna/diceapp/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.josanna.diceapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var randomNumberTxt : TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        randomNumberTxt = findViewById<TextView>(R.id.txt_random_number)
+        val rollButton = findViewById<Button>(R.id.btn_roll_dice)
+
+        rollButton.setOnClickListener{
+            rollDice()
+        }
+    }
+    fun rollDice() {
+
+        val randomNumber = (1..6).random()
+        randomNumberTxt.text = randomNumber.toString()
+    }
+
+}
